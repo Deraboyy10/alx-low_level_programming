97df5de4f01c4e8e1f4634f26b6b1779b148492b
Deraboyy10 <augustineidara2@gmai.com>
2023-08-10 18:03:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - A program that prints a line with puts functions
+ * Return: 0 if (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-{
+}
